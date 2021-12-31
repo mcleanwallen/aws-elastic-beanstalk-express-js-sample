5bf9de078bf77a0d7cebe656ccd1aa0f79a3059e
mcleanwallen <all17053@byui.edu>
2021-12-30 22:18:32 -0700

    Added Allen

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My name is Mac!'));
+app.get('/', (req, res) => res.send('My name is Mac Allen!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
